30a50588c9d2f8f0d7daace1dea2f4f04d4295f5
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-29 17:17:50 +0300

    lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png"
+                     alt="image"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"
+                         alt="background"/>
+                </div>
+                <div>ava+description</div>
+                <div>My post
+                    <div>
+                        New post
+                    </div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
 
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
-
-export default App;
