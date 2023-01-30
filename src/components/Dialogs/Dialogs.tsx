d1b5310bdd79c2a133f712292345b32bdc016650
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-30 15:16:57 +0300

    part 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={`${s.dialog} ${s.active}`}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Veronika
+                </div>
+                <div className={s.dialog}>
+                    Sofia
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Who are you</div>
+                <div className={s.message}>Fuck off</div>
+            </div>
         </div>
     );
 };
