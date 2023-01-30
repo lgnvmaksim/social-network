bd1241a0c3f989501c681b5fed825e56f13cd62a
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-30 08:01:52 +0300

    lesson 12

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png"
+                 alt="image"/>
+        </header>
+    );
+};
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Profile = () => {
+     return (
+         <div className='content'>
+             <div>
+                 <img src="https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"
+                      alt="background"/>
+             </div>
+             <div>ava+description</div>
+             <div>My post
+                 <div>
+                     New post
+                 </div>
+                 <div>post1</div>
+                 <div>post2</div>
+             </div>
+
+         </div>
+    );
+};
