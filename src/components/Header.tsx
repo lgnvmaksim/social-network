d82af17b9e2542a078058d99123aa28f495bd05a
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-30 08:45:30 +0300

    14

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css'
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://www.freepnglogos.com/uploads/company-logo-png/company-logo-transparent-png-19.png"
                  alt="image"/>
         </header>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 export const Profile = () => {
-     return (
-         <div className='content'>
-             <div>
-                 <img src="https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"
-                      alt="background"/>
-             </div>
-             <div>ava+description</div>
-             <div>My post
-                 <div>
-                     New post
-                 </div>
-                 <div>post1</div>
-                 <div>post2</div>
-             </div>
-
-         </div>
+    return (
+        <div className={s.content}>
+            <div>
+                <img src="https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"
+                     alt="background"/>
+            </div>
+            <div>ava+description</div>
+            <div>My post
+                <div>
+                    New post
+                </div>
+                <div className={s.posts}>
+                    <div className={s.item}>post1</div>
+                    <div className={s.item}>post2</div>
+                </div>
+            </div>
+        </div>
     );
 };
