f032dd7ddd8d109926a8d0bc1f4347c1abc437ed
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-30 10:07:50 +0300

    part 18

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src="https://weblinks.ru/wp-content/uploads/2022/08/0d6de7af1701b7f6ff551d4474ced401-730x578.jpeg"
                      alt="background"/>
