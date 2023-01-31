9d4a076162cc3d1f9cbffc61fa28597cd2d88f4e
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-31 10:07:58 +0300

    part 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -4,13 +4,15 @@ import s from './MyPosts.module.css'
 import {ProfilePageType} from "../../../redux/state";
 
 
-
-
-
 export const MyPosts = (props: ProfilePageType) => {
+    let postsElements = props.posts.map(el => <Post message={el.message} likeCount={el.likesCount} key={el.id}/>)
 
 
-    let postsElements = props.posts.map(el=><Post message={el.message} likeCount={el.likesCount} key={el.id}/>)
+    let postMessageRef = React.createRef<HTMLTextAreaElement>()
+
+    const addPost = () => {
+        alert(postMessageRef.current?.value )
+    }
 
     return (
         <div className={s.postsBlock}>
@@ -19,11 +21,11 @@ export const MyPosts = (props: ProfilePageType) => {
             </h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={postMessageRef}></textarea>
                 </div>
-              <div>
-                  <button>Add post</button>
-              </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
+                </div>
 
             </div>
             <div className={s.posts}>
